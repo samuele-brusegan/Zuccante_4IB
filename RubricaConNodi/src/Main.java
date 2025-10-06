094483ae4cf1dc08b28a9c11b1aefa9577ca3466
samuele.brusegan <samuele.brusegan@itiszuccante.edu.it>
2025-10-06 08:53:05 +0200

    Finito esercizio Rubrica

--- RubricaConNodi/src/Main.java
+++ RubricaConNodi/src/Main.java
@@ -1,4 +1,7 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
+
+import static utilities.General.input;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,17 +22,71 @@ public class Main {
         }
 
         //Todo:
+        menu(r);
+    }
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("==== Menu' ====");
+    public static void menu(Rubrica r) {
+        System.out.println("\n\n==== Menu' ====");
         System.out.println("1. Aggiungi contatto");
         System.out.println("2. Rimuovi contatto");
         System.out.println("3. Cerca contatto");
         System.out.println("4. Visualizza lista");
+        System.out.println("5. Esci");
+        System.out.println();
 
+        String in = input("Seleziona la funzione: ", "");
+        System.out.println();
 
+        if (in.equals("5")) {
+            System.out.println("Arrivederci e a presto!");
+            return;
+        }
 
+        switch (in) {
+            case "1" -> {
+                System.out.println("== Aggiungi contatto ==");
+                String name    = input("Inserisci il nome: ", "");
+                String surname = input("Inserisci il cognome: ", "");
+                String email   = input("Inserisci l'email: ", "");
+                String phone   = input("Inserisci il tele-phone: ", "");
 
+                Persona p = new Persona(name, surname, email, phone);
 
+                r.add(p);
+
+                System.out.println("\nContatto aggiunto correttamente!");
+            }
+            case "2" -> {
+                System.out.println("== Rimuovi contatto ==");
+
+                String name    = input("Inserisci il nome: ", "");
+                String surname = input("Inserisci il cognome: ", "");
+
+                boolean b = r.delete(surname, name);
+                if  (b) System.out.println("Contatto eliminato correttamente!");
+                else    System.out.println("Contatto non eliminato correttamente!");
+
+            }
+            case "3" -> {
+                System.out.println("== Cerca contatto ==");
+                String name    = input("Inserisci il nome: ", "");
+                String surname = input("Inserisci il cognome: ", "");
+
+                int pos = r.search(surname, name);
+
+                if (pos >= 0) {
+                    System.out.println("Il contatto \""+ surname + " " + name + "\" è il " + (pos+1)+ "° nella rubrica");
+                } else {
+                    System.out.println("Il contatto non è presente in rubrica");
+                }
+            }
+            case "4" -> {
+                System.out.println("== Visualizza lista ==");
+
+                System.out.println(r);
+            }
+            default -> System.err.println("Opid non valido");
+        }
+        menu(r);
     }
 }
